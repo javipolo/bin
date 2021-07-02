b787578e95205e24c5d584287aa9d8c388691db1
Javi Polo <javipolo@drslump.org>
2021-07-02 14:48:09 +0200

    Add ability to clone pod and to set safe-to-evict to false

--- kube-debug-pod.sh
+++ kube-debug-pod.sh
@@ -4,6 +4,8 @@ debug_image=javipolo/ubuntu-debug:latest
 
 [ "$1" == "-n" ] && { dryrun=true; shift 1; }
 [ "$1" == "-d" ] && { image=${debug_image}; shift 1; }
+[ "$1" == "-e" ] && { safe_to_evict=false; shift 1; }
+[ "$1" == "-p" ] && { type=pod; shift 1; }
 [ "$1" == "-i" ] && { image=$2; shift 2; }
 
 deploy_name=$1
@@ -14,6 +16,7 @@ pod_name='{ "name": "'$pod_new_name'" }'
 pod_labels='{ "name": "'$pod_new_name'", "debug": "'$USER'" }'
 pod_command='{ "command": ["tail", "-f", "/dev/null"] }'
 [ "$image" ] && pod_image='{ "image": "'${image}'"}' || pod_image=null
+[ "$safe_to_evict" == "false" ] && pod_no_evict='{ "cluster-autoscaler.kubernetes.io/safe-to-evict": "false" }' || pod_no_evict=null
 
 apply_command="kubectl apply -f -"
 [ "$dryrun" == "true" ] && apply_command=$(command -v json2yaml > /dev/null && echo json2yaml || echo cat)
@@ -25,6 +28,8 @@ Creates a debugging pod using a deployment as base
 Usage: $0 [-n] [-d] [-i image:name] <deploy-name>
         -n        dry run       - Do nothing, just print the generated json/yaml
         -d        debug image   - Use defaut debug image in all the containers
+        -e                      - Set the safe-to-evict to false for this pod
+        -p                      - Use a pod instead of a deployment as template
         -i image  custom image  - Use specific image in all the containers
 
         default debug image: $debug_image
@@ -50,6 +55,7 @@ pod_amend(){
     jq ". |
         .metadata += $pod_name |
         .metadata.labels += $pod_labels |
+        .metadata.annotations += $pod_no_evict |
         .spec.containers[] += $pod_image |
         .spec.containers[] += $pod_command
        "
@@ -57,4 +63,8 @@ pod_amend(){
 
 [ "$deploy_name" ] || usage
 
-kubectl get deploy $1 -o json | deploy_to_pod | pod_cleanup | pod_amend | $apply_command
+if [ "$type" == "pod" ]; then
+    kubectl get pod $1 -o json | pod_cleanup | pod_amend | $apply_command
+else
+    kubectl get deploy $1 -o json | deploy_to_pod | pod_cleanup | pod_amend | $apply_command
+fi
