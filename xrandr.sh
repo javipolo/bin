2c627a88a3659d29a7a210b3648c27edd2e27d8d
Javi Polo <javipolo@redhat.com>
2022-01-18 11:41:38 +0100

    Update xrandr to match new screens setup

--- xrandr.sh
+++ xrandr.sh
@@ -1,10 +1,17 @@
-LAPTOP=$(xrandr|grep ' primary'| grep ' connected'|cut -d ' ' -f 1)
-MONITOR=$(xrandr|grep -v ' primary'| grep ' connected'|cut -d ' ' -f 1)
-MMODE=1920x1080
-LMODE=1920x1200
+#LAPTOP=$(xrandr|grep ' primary'| grep ' connected'|cut -d ' ' -f 1)
+#MONITOR=$(xrandr|grep -v ' primary'| grep ' connected'|cut -d ' ' -f 1)
 
-if [ "$MONITOR" ]; then
-    extra="--output $MONITOR --left-of $LAPTOP --mode ${MMODE}"
+LAPTOP=eDP1
+LMODE=disabled
+PRIMARY=DP-1-3
+PMODE=1920x1080
+SECONDARY=DP-1-1
+SMODE=1920x1080
+
+if [ "$(xrandr | grep $SECONDARY |grep ' connected')" ]; then
+    extra="--output $SECONDARY --left-of $PRIMARY --mode ${SMODE} --output $LAPTOP --off"
+else
+    PRIMARY=$LAPTOP
 fi
 
-xrandr --output $LAPTOP --mode ${LMODE} $extra
+xrandr --output $PRIMARY --primary --mode ${PMODE} $extra
