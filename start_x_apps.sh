86b6512354f7a3a4abb541f1278ed4afe6dd6568
Javi Polo <jpolo@redhat.com>
2022-08-05 12:01:24 +0200

    Provide a wrapper to google-chrome

--- start_x_apps.sh
+++ start_x_apps.sh
@@ -2,11 +2,11 @@
 
 PATH=$PATH:/home/javipolo/bin/local
 
-google-chrome --profile-directory=Default &
+chrome --profile-directory=Default &
 pgrep -i slack || slack &
 pgrep -i spotify || spotify &
 pgrep -i Telegram || Telegram &
-google-chrome --profile-directory="Profile 1" &
+chrome --profile-directory="Profile 1" &
 
 X.sh
 sleep 10
