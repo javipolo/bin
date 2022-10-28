30f40a1be555fd012731c98cea418fa6c64f6dc1
Javi Polo <jpolo@redhat.com>
2022-10-28 19:00:44 +0200

    Simplify and refactor all screen handling/detection

--- rearrange_windows.sh
+++ rearrange_windows.sh
@@ -2,76 +2,44 @@
 # Set windows to standard position/desktop
 
 # Requires
-# - xdotool (used to search window ids)
 # - wmctrl  (used to move and resize windows)
-# - xrandr  (used to find out if we are using multiple screens and to get
-#            current resolution)
+# - xrandr  (used to find out if we are using multiple screens and to get current resolution)
 
-apps_left=""
-apps_center="terminal"
-apps_right="telegram slack skype spotify chrome"
-screens=${*:-left center right}
+screen_1="terminal"
+screen_2="telegram slack skype spotify chrome"
+laptop_screen_name=eDP-1
 
 get_wids(){
-    xdotool search --maxdepth 2 --onlyvisible --name "$1" || echo NULL
-}
-
-move_to_desktop(){
-    for winid in $(get_wids "$1"); do
-        wmctrl -i -r $winid -t $2
-    done
+    wmctrl -l| grep -i "$1" | cut -d ' ' -f 1
 }
 
 rearrange(){
-    local app screen laptop_lid n num_displays resolution width resize_width tmp_w tmp_h
-    declare -i num_displays
+    local app screen tmp_w tmp_h screen_start screen_start_x screen_start_y
 
     app="$1"
     screen=$2
-    laptop_lid=$(awk '{print $NF}' /proc/acpi/button/lid/LID/state)
-
-    if [ "$laptop_lid" == "closed" ]; then
-        case $screen in
-            center) n=0;;
-            right) n=1;;
-            # If lid is closed, left screen stuff goes to right screen
-            left) n=1;;
-        esac
-    else
-        case $screen in
-            left) n=0;;
-            center) n=1;;
-            right) n=2;;
-        esac
-    fi
-
-    num_displays=$(xrandr | grep -c '*')
-    resolution=$(xdpyinfo | awk '/dimensions/{print $2}')
-    width=$(echo $resolution | cut -d x -f 1)
-    resize_width=$(( n * ( width/num_displays) + 100))
     tmp_w=10
     tmp_h=10
+    lid_state=$(awk '{print $NF}' /proc/acpi/button/lid/LID/state)
+    if [ "$lid_state" == "closed" ]; then
+      screen_start=$(xrandr| grep -v "^$laptop_screen_name" | grep ' connected' | cut -d ' ' -f 3 | cut -d + -f 2,3 | sort | head -n $screen | tail -n1)
+    else
+      screen_start=$(xrandr|grep ' connected' | cut -d ' ' -f 3 | cut -d + -f 2,3 | sort | head -n $screen | tail -n1)
+    fi
+    screen_start_x=$(cut -d + -f 1 <<< $screen_start)
+    screen_start_y=$(cut -d + -f 2 <<< $screen_start)
 
-    for winid in $(get_wids "$app"); do
-        if [ "$winid" != "NULL" ]; then
-            wmctrl -i -r $winid -b remove,maximized_vert,maximized_horz
-            wmctrl -i -r $winid -e 0,$resize_width,0,$tmp_w,$tmp_h
-            wmctrl -i -r $winid -b add,maximized_vert,maximized_horz
-            wmctrl -i -r $winid -b add,sticky
-        fi
+    get_wids "$app" | while read -r winid; do
+      wmctrl -i -r $winid -b remove,maximized_vert,maximized_horz
+      wmctrl -i -r $winid -e 0,$screen_start_x,$screen_start_y,$tmp_w,$tmp_h
+      wmctrl -i -r $winid -b add,maximized_vert,maximized_horz
     done
 }
 
-declare -i num_displays
-num_displays=$(xrandr | grep -c '*')
-
-# Only do things if we are using more than 1 display
-if [ $num_displays -gt 1 ]; then
-    for screen in $screens; do
-        apps=apps_${screen}
-        for name in ${!apps}; do
-            rearrange $name $screen
-        done
+num_displays=$(xrandr | grep -c ' connected')
+for screen in $(seq $num_displays); do
+    apps=screen_${screen}
+    for name in ${!apps}; do
+        rearrange $name $screen
     done
-fi
-
+done
