7530e8cd77bb1fe90151f7d233c02112abfaf8e0
Javi Polo <jpolo@redhat.com>
2024-05-27 16:34:49 +0200

    Auto set zoom for chrome depending on location

--- start_x_apps.sh
+++ start_x_apps.sh
@@ -2,11 +2,12 @@
 
 PATH=$PATH:/home/javipolo/bin/local
 
-google-chrome --profile-directory=Default &
+wmctrl -l | grep Chrome || ~/bin/run_chrome --profile-directory=Default &
 pgrep -i slack || slack &
-pgrep -i spotify || spotify &
+pgrep -i spotify || flatpak run com.spotify.Client &
+# pgrep -i signal || flatpak run org.signal.Signal &
 pgrep -i Telegram || Telegram &
-google-chrome --profile-directory="Profile 1" &
+wmctrl -l | grep -c Chrome | grep -x 2 || ~/bin/run_chrome --profile-directory="Profile 1" &
 
 X.sh
 sleep 10
